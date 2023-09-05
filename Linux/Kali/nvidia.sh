fbce63308bc74b88181d65d0cf7d90f9f4f63b09
҉αkα x⠠⠵ <32862241+4k4xs4pH1r3@users.noreply.github.com>
2023-09-05 19:07:55 +0000

    Update nvidia.sh

--- Linux/Kali/nvidia.sh
+++ Linux/Kali/nvidia.sh
@@ -69,3 +69,4 @@ dpkg -i *.deb
 apt --fix-broken install -y
 dpkg -i *.deb
 apt install aptitude -y && aptitude install locales -y && locale-gen en_US.UTF-8 && localedef -i en_US -f UTF-8 en_US.UTF-8 && export LANGUAGE=en_US.UTF-8 && export LANG=en_US.UTF-8 && export LC_ALL=en_US.UTF-8 && locale-gen en_US.UTF-8 && apt-get update -y && apt-get full-upgrade -y && aptitude install wget -y && apt-get install python3 python3-venv python3-pip -y && apt-get autoclean && apt-get clean cache && apt update -y && apt-get full-upgrade -y
+reboot
