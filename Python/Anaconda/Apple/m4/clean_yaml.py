91f8d943f94a6bcee586e2d3d05016865c8d2518
҉αkα x⠠⠵ <32862241+4k4xs4pH1r3@users.noreply.github.com>
2024-11-28 09:57:30 -0500

    Update clean_yaml.py
    Signed-off-by: ҉αkα x⠠⠵ <32862241+4k4xs4pH1r3@users.noreply.github.com>

--- Python/Anaconda/Apple/m4/clean_yaml.py
+++ Python/Anaconda/Apple/m4/clean_yaml.py
@@ -4,20 +4,24 @@ import yaml
 with open('x.yaml', 'r') as f:
     data = yaml.safe_load(f)
 
-# Process dependencies, handling both strings and dictionaries
+# Process regular dependencies
 new_dependencies = []
 for item in data['dependencies']:
     if isinstance(item, str):
         package_name = item.split('=')[0]
         new_dependencies.append(package_name)
     elif isinstance(item, dict):
-        # Assuming each dictionary has one key-value pair
-        package_name = list(item.keys())[0].split('=')[0] 
+        package_name = list(item.keys())[0].split('=')[0]
         new_dependencies.append(package_name)
-
-# Update the dependencies in the data
 data['dependencies'] = new_dependencies
+
+# Process pip dependencies
+new_pip_dependencies = []
+for item in data['pip']:
+    package_name = item.split('==')[0]
+    new_pip_dependencies.append(package_name)
+data['pip'] = new_pip_dependencies
 
 # Write the modified data back to the YAML file
 with open('x.yaml', 'w') as f:
-    yaml.dump(data, f, indent=2)  # Added indent for better readability
+    yaml.dump(data, f, indent=2)
