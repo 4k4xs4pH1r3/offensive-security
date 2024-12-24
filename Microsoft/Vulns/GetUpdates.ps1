8e5cb17d78d7ad870ba0a1e073edb31b937377bd
҉αkα x⠠⠵ <32862241+4k4xs4pH1r3@users.noreply.github.com>
2024-12-24 13:55:16 -0500

    Update GetUpdates.ps1
    Signed-off-by: ҉αkα x⠠⠵ <32862241+4k4xs4pH1r3@users.noreply.github.com>

--- Microsoft/Vulns/GetUpdates.ps1
+++ Microsoft/Vulns/GetUpdates.ps1
@@ -1,4 +1,6 @@
 Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force
+or
+Set-ExecutionPolicy RemoteSigned -Force
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock" /t REG_DWORD /f /v "AllowDevelopmentWithoutDevLicense" /d "1"
 irm get.scoop.sh -outfile 'install.ps1'
 .\install.ps1 -RunAsAdmin -ScoopDir 'C:\Base\' -ScoopGlobalDir 'C:\Global' -NoProxy
